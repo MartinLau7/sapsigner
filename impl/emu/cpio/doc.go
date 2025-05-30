62aa99f972333353ac757179829cf2f4c0216294
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2025-05-29 17:39:29 -0700

    add acknowledgment

---
+++ impl/emu/cpio/doc.go
@@ -0,0 +1,6 @@
+// Package cpio implements a stream reader of CPIO archives with POSIX.1 format,
+// also known as ODC or Old Character Format.
+//
+// This package is based on [github.com/korylprince/go-cpio-odc] by Kory Prince,
+// which served as the inspiration and reference for this implementation.
+package cpio
